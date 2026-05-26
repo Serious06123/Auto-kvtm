953eb53e80de1b3cf8f9461ec0edc37c5ff1c406
Serious06123 <kheau779@gmail.com>
2026-05-26 08:55:37 +0700

    feat: update 2.1.2

--- server/src/services/log.js
+++ server/src/services/log.js
@@ -1,9 +1,15 @@
-const { appendFileSync, writeFileSync } = require('fs')
-const { resolve } = require('path')
+const { appendFileSync, writeFileSync, existsSync, mkdirSync } = require('fs')
+const { resolve, dirname } = require('path')
 const moment = require('moment')
 
 const errFilePath = resolve(__dirname, '../../../log/err.txt')
 const infoFilePath = resolve(__dirname, '../../../log/info.txt')
+
+// Tu dong tao thu muc log neu chua co
+const logDir = dirname(errFilePath)
+if (!existsSync(logDir)) {
+    mkdirSync(logDir, { recursive: true })
+}
 
 function logErrMsg(msg) {
     appendFileSync(errFilePath, moment().format('LTS') + ' : ')
